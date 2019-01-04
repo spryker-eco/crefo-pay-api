cd4fd809d71391f32e09bd70a14146cdeafb714d
Aleksey Kotsuba <aleksey.kotsuba@spryker.com>
2019-01-04 07:08:41 +0000

    ECO-2344: Update transfers and console command.

--- src/SprykerEco/Zed/CrefoPayApi/Communication/Console/CreateTransactionCallConsole.php
+++ src/SprykerEco/Zed/CrefoPayApi/Communication/Console/CreateTransactionCallConsole.php
@@ -7,6 +7,12 @@
 
 namespace SprykerEco\Zed\CrefoPayApi\Communication\Console;
 
+use ArrayObject;
+use Generated\Shared\Transfer\CrefoPayApiAddressTransfer;
+use Generated\Shared\Transfer\CrefoPayApiAmountTransfer;
+use Generated\Shared\Transfer\CrefoPayApiBasketItemTransfer;
+use Generated\Shared\Transfer\CrefoPayApiCreateTransactionRequestTransfer;
+use Generated\Shared\Transfer\CrefoPayApiPersonTransfer;
 use Generated\Shared\Transfer\CrefoPayApiRequestTransfer;
 use Spryker\Zed\Kernel\Communication\Console\Console;
 use Symfony\Component\Console\Input\InputInterface;
@@ -45,10 +51,76 @@ class CreateTransactionCallConsole extends Console
         echo json_encode($response->toArray(true, true), JSON_PRETTY_PRINT);
     }
 
+    /**
+     * @return \Generated\Shared\Transfer\CrefoPayApiRequestTransfer
+     */
     protected function createRequestTransfer(): CrefoPayApiRequestTransfer
     {
         $request = new CrefoPayApiRequestTransfer();
+        $request->setCreateTransactionRequest($this->createRequestCallTransfer());
 
         return $request;
+    }
+
+    protected function createRequestCallTransfer()
+    {
+        return (new CrefoPayApiCreateTransactionRequestTransfer())
+            ->setMerchantID(265)
+            ->setStoreID('sprykerdevelEUR')
+            ->setOrderID('test-order-id')
+            ->setUserID('user-id')
+            ->setIntegrationType('API')
+            ->setAutoCapture(false)
+            ->setMerchantReference('reference')
+            ->setContext('ONLINE')
+            ->setUserType('PRIVATE')
+            ->setUserRiskClass(0)
+            ->setUserIpAddress('127.0.0.1')
+            ->setUserData(
+                (new CrefoPayApiPersonTransfer())
+                    ->setSalutation('M')
+                    ->setName('Aleksey')
+                    ->setSurname('Kotsuba')
+                    ->setDateOfBirth('1988-10-22')
+                    ->setEmail('aleksey.kotsuba@spryker.com')
+                    ->setPhoneNumber('0380937880368')
+            )
+            ->setBillingAddress(
+                (new CrefoPayApiAddressTransfer())
+                    ->setZip('123456')
+                    ->setCountry('DE')
+                    ->setCity('Berlin')
+                    ->setStreet('Test street')
+                    ->setNo('12')
+            )
+            ->setAmount(
+                (new CrefoPayApiAmountTransfer())
+                    ->setAmount(1922)
+                    ->setVatAmount(19)
+                    ->setVatRate(10.00)
+            )
+            ->setBasketItems($this->createBasket())
+            ->setLocale('EN');
+    }
+
+    protected function createBasket()
+    {
+        $basket = new ArrayObject();
+        $basket->append(
+                (new CrefoPayApiBasketItemTransfer())
+                    ->setBasketItemAmount(
+                        (new CrefoPayApiAmountTransfer())
+                            ->setAmount(1922)
+                            ->setVatAmount(19)
+                            ->setVatRate(10.00)
+                    )
+                    ->setBasketItemCount(1)
+                    ->setBasketItemID('sku')
+                    ->setBasketItemText('Description')
+                    ->setBasketItemRiskClass(0)
+                    ->setBasketItemType('DEFAULT')
+            );
+
+        return $basket;
     }
 }
