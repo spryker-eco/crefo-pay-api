db2752e1eab214cfeeb596394b52af1e225b6912
Aleksey Kotsuba <aleksey.kotsuba@spryker.com>
2019-07-11 14:37:53 +0000

    ECO-2295: Added api to module constants.

--- src/SprykerEco/Shared/CrefoPayApi/CrefoPayApiConstants.php
+++ src/SprykerEco/Shared/CrefoPayApi/CrefoPayApiConstants.php
@@ -12,13 +12,67 @@ namespace SprykerEco\Shared\CrefoPayApi;
  */
 interface CrefoPayApiConstants
 {
+    /**
+     * Specification:
+     *  - Url for createTransaction API requests.
+     *
+     * @api
+     */
     public const CREATE_TRANSACTION_API_ENDPOINT = 'CREFO_PAY_API:CREATE_TRANSACTION_API_ENDPOINT';
+
+    /**
+     * Specification:
+     *  - Url for reserve API requests.
+     *
+     * @api
+     */
     public const RESERVE_API_ENDPOINT = 'CREFO_PAY_API:RESERVE_API_ENDPOINT';
+
+    /**
+     * Specification:
+     *  - Url for capture API requests.
+     *
+     * @api
+     */
     public const CAPTURE_API_ENDPOINT = 'CREFO_PAY_API:CAPTURE_API_ENDPOINT';
+
+    /**
+     * Specification:
+     *  - Url for cancel API requests.
+     *
+     * @api
+     */
     public const CANCEL_API_ENDPOINT = 'CREFO_PAY_API:CANCEL_API_ENDPOINT';
+
+    /**
+     * Specification:
+     *  - Url for refund API requests.
+     *
+     * @api
+     */
     public const REFUND_API_ENDPOINT = 'CREFO_PAY_API:REFUND_API_ENDPOINT';
+
+    /**
+     * Specification:
+     *  - Url for finish API requests.
+     *
+     * @api
+     */
     public const FINISH_API_ENDPOINT = 'CREFO_PAY_API:FINISH_API_ENDPOINT';
 
+    /**
+     * Specification:
+     *  - Private key provided by CrefoPay string value. Used for MAC calculation.
+     *
+     * @api
+     */
     public const PRIVATE_KEY = 'CREFO_PAY_API:PRIVATE_KEY';
+
+    /**
+     * Specification:
+     *  - Public key provided by CrefoPay string value. Used for sign requests.
+     *
+     * @api
+     */
     public const PUBLIC_KEY = 'CREFO_PAY_API:PUBLIC_KEY';
 }
